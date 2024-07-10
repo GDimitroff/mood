17f554719a08981436cb49716442cdff9534c64d
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-07-10 10:18:50 +0300

    add db scripts

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,4 @@
-import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+import { integer, pgTable, serial, text, timestamp, uuid } from 'drizzle-orm/pg-core'
 
 export const usersTable = pgTable('users_table', {
   id: serial('id').primaryKey(),
@@ -24,3 +24,7 @@ export type InsertUser = typeof usersTable.$inferInsert
 export type SelectUser = typeof usersTable.$inferSelect
 export type InsertPost = typeof postsTable.$inferInsert
 export type SelectPost = typeof postsTable.$inferSelect
+
+export const users = pgTable('users', {
+  id: uuid('id').primaryKey().defaultRandom(),
+})
